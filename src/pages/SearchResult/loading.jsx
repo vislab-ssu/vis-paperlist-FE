5b8dca32b4985a4e0596a7e3f3814595eaa2fd63
wj-cosmos57 <wooju_0507@naver.com>
2024-03-18 10:44:34 +0900

    [comment] change comment

--- src/pages/SearchResult/loading.jsx
+++ src/pages/SearchResult/loading.jsx
@@ -5,22 +5,11 @@ import Lottie from "lottie-react";
 import loadingAnimation from "../../assets/lottie/Animation_Load.json";
 
 const Loading = () => {
-  // const defaultOptions = {
-  //   //예제1
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: LottieData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
-
   return (
     <div
       style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}
     >
       <Lottie
-        // options={defaultOptions}
         style={{width: 500, height: 500, background: "transparent"}}
         animationData={loadingAnimation}
       ></Lottie>
